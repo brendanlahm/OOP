4f48dbd6ad4580a66ab8611818a07ae174d151d0
brendanlahm <65281324+brendanlahm@users.noreply.github.com>
2025-07-12 00:03:33 +0200

    VehicleFleetTree is a full BST with max 2 successor nodes

--- BST/src/Main.java
+++ BST/src/Main.java
@@ -5,9 +5,17 @@ public class Main {
     // Object-Oriented Method
     public static void main(String[] args) {
 
+    // Initiate the tree
     VehicleFleetTree tree = new VehicleFleetTree();
+
+    // Add cars to the tree
     tree.add(301, "Hyundai", "Sonata", 2013, null, null);
-    tree.add(302, "Hyundai", "Accord", 2024, null, null);
+    tree.add(309, "Hyundai", "Accord", 2024, null, null);
+    tree.add(291, "Honda", "Civic", 2001, null, null);
+    tree.add(289, "Honda", "Odyssey", 2016, null, null);
+    tree.add(292, "Lamborghini", "Gallardo", 2014, null, null);
+    tree.add(303, "Lamborghini", "Huracan", 2023, null, null);
+    tree.add(311, "Lamborghini", "Aventador", 2020, null, null);
 
     }
 
--- BST/src/VehicleFleetTree.java
+++ BST/src/VehicleFleetTree.java
@@ -6,9 +6,15 @@ public class VehicleFleetTree {
     // Private attributes
     private Vehicle root;
 
+    // Constructor
+    public VehicleFleetTree() {
+        root = null;
+    }
+
+    // Method for adding a vehicle
     public void add(int vehicleId, String make, String model, int year, Vehicle left, Vehicle right) {
 
-        Vehicle v = new Vehicle(vehicleId, make, model, year, left, right); // Constructor
+        Vehicle v = new Vehicle(vehicleId, make, model, year, left, right);
 
         // Setting the root vehicle
         if (root == null) {
@@ -16,16 +22,34 @@ public class VehicleFleetTree {
             System.out.println("Root vehicle: " + root.getVehicleId() + ", " + root.getMake() + ", " + root.getModel() + ", " + root.getYear());
 
             // Setting vehicle to the left of root
-        } else if (v.vehicleId < root.getVehicleId()) {
+        } else if (v.vehicleId < root.getVehicleId() && root.left == null) {
             root.left = v;
-            System.out.println("Added vehicle (left): " + root.left.getVehicleId() + ", " + root.left.getMake() + ", " + root.left.getModel() + ", " + root.left.getYear());
+            System.out.println("Added vehicle (root -> left): " + root.left.getVehicleId() + ", " + root.left.getMake() + ", " + root.left.getModel() + ", " + root.left.getYear());
+
+            // Setting vehicle to left -> left
+        } else if (root.left != null && v.vehicleId < root.getVehicleId() && v.vehicleId < root.left.getVehicleId()) {
+            root.left.left = v;
+            System.out.println("Added vehicle (root -> left -> left): " + root.left.left.getVehicleId() + ", " + root.left.left.getMake() + ", " + root.left.left.getModel() + ", " + root.left.left.getYear());
+
+            // Setting vehicle to left -> right
+        } else if (root.left != null && v.vehicleId < root.getVehicleId() && v.vehicleId > root.left.getVehicleId()) {
+            root.left.right = v;
+            System.out.println("Added vehicle (root -> left -> right): " + root.left.right.getVehicleId() + ", " + root.left.right.getMake() + ", " + root.left.right.getModel() + ", " + root.left.right.getYear());
 
             // Setting vehicle to the right of root
-        } else if (v.vehicleId > root.getVehicleId()) {
+        } else if (v.vehicleId > root.getVehicleId() && root.right == null) {
             root.right = v;
-            System.out.println("Added vehicle (right): " + root.right.getVehicleId() + ", " + root.right.getMake() + ", " + root.right.getModel() + ", " + root.right.getYear());
+            System.out.println("Added vehicle (root -> right): " + root.right.getVehicleId() + ", " + root.right.getMake() + ", " + root.right.getModel() + ", " + root.right.getYear());
+
+        // Setting vehicle to right -> left
+        } else if (root.right != null && v.vehicleId > root.getVehicleId() && v.vehicleId < root.right.getVehicleId()) {
+            root.right.left = v;
+            System.out.println("Added vehicle (root -> right -> left): " + root.right.left.getVehicleId() + ", " + root.right.left.getMake() + ", " + root.right.left.getModel() + ", " + root.right.left.getYear());
+
+        // Setting vehicle to right -> right
+        } else if (root.right != null && v.vehicleId > root.getVehicleId() && v.vehicleId > root.right.getVehicleId()) {
+            root.right.right = v;
+            System.out.println("Added vehicle (root -> right -> right): " + root.right.right.getVehicleId() + ", " + root.right.right.getMake() + ", " + root.right.right.getModel() + ", " + root.right.right.getYear());
         }
-
     }
-
 }

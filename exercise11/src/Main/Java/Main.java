15eab1948aa2d8bf572bbc0769d7acf21a6cf2d6
brendanlahm <65281324+brendanlahm@users.noreply.github.com>
2025-12-14 22:10:14 +0100

    Added a stream for averaging goals per match in Exercise 11

--- exercise11/src/Main/Java/Main.java
+++ exercise11/src/Main/Java/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.stream.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,19 +7,27 @@ public class Main {
         try {
             Bundesliga bundesliga = Bundesliga.loadFromResource(); // Load Data
             System.out.println("Clubs loaded: " + bundesliga.clubs.size());
-            System.out.println("Games loaded: " + bundesliga.games.size());
+            System.out.println("Games loaded: " + bundesliga.games.size() + "\n");
 
             // Print all club names using stream
-            bundesliga.clubs.values()
-                    .stream()
-                    .map(Club::getName)
-                    .forEach(System.out::println);
+//            bundesliga.clubs.values() // .values() works for Maps only?
+//                    .stream()
+//                    .map(Club::getName)
+//                    .forEach(System.out::println);
 
-            // Print all club IDs, names & leagues using lambda (clubs is a Map w/ values!)
-            bundesliga.clubs.values().forEach(club -> System.out.println(club.getId() + ", " + club.getName() + ", " + club.getLeague()));
+            // Print all club IDs, names & leagues using Lambda (clubs is a Map w/ values!)
+            bundesliga.clubs.values().forEach(club -> System.out.println(club.getId() + ". " + club.getName() + ", League: " + club.getLeague()));
 
-            // Print all games names using lambda (games is a List w/o values!)
+            // Print all games names using Lambda (games is a List w/o values!)
             //bundesliga.games.forEach(game -> System.out.println(game.getClubIdHome() + " vs " + game.getClubIdGuest()));;
+
+            // Print average goals in a match using stream
+            double averageGoals = bundesliga.games.stream()
+                    .mapToInt(game -> game.getGoalsHome() + game.getGoalsGuest())
+                    .average()
+                    .orElse(0.0);
+            System.out.println("\nAverage goals per match: " + averageGoals);
+
 
         } catch (IOException e) {
             System.err.println("Failed to load Bundesliga data: " + e.getMessage());
